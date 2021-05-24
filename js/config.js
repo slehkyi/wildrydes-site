098cc3e942e97c03d0a3812654db5e7c91f2fc4b
slehkyi <sergii.lehkii@gmail.com>
2021-05-24 20:24:56 +0200

    add users

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-3_s8rLJlNJ2', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6s5c35o96v9dk4n2rb1hokvq3k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-3' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
